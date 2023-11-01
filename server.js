9b61a7f28cd7621562ce2021f453b9fbe00f35f3
marshall-bits <kz_@hotmail.es>
2023-11-01 13:15:07 +0100

    add nodemon

--- server.js
+++ server.js
@@ -6,17 +6,46 @@ const server = express();
 server.set("views", __dirname + "/views");
 server.set("view engine", "hbs"); // here we are adding a view engine called hbs
 
-server.get("/", (req, res) => {
+async function getPokemons() {
+    const data = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    const jsonData = await data.json();
+    const { results } = jsonData;
+    return results;
+}
+
+server.get(["/", "/index"], async (req, res) => { // we can add an array to use multiple routes for the same thing
     //res.sendFile(__dirname + "/index.html"); // here we don't need the DOT because we are in the middle of a path
-    res.render("index"); // we don't need to add the extension html or hbs now because we have the engine
+    const pokemonArray = await getPokemons();
+    console.log(pokemonArray);
+    const data = {
+        name: "Clara",
+        role: "Boomer",
+        bootcamp: "",
+        favouriteFood: pokemonArray,
+        isPaid: false
+    }
+    /* 
+    BACKLOG:
+        const moreData = {
+            name: "Marc",
+            role: "Tiger",
+            bootcamp: "Web dev"
+        } 
+    */
+
+    res.render("index", data); // we don't need to add the extension html or hbs now because we have the engine
 });
 
-server.get("/about", (req,res)=>{
+server.get("/about", (req, res) => {
     res.render("about");
-})
+});
 
-server.get("*",(req,res)=>{
+server.get("/contacts", (req, res) => {
+    res.render("contacts");
+});
+
+server.get("*", (req, res) => {
     res.render("error");
-})
+});
 
 server.listen(port, () => console.log(`SERVER running on port ${port}`));
